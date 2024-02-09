a3baf728819495a61f355977eb9788202092a205
gowrig410 <gowriganesh410@gmail.com>
2024-02-09 12:23:39 +0530

    authentication

--- controllers/userRouter.js
+++ controllers/userRouter.js
@@ -29,5 +29,36 @@ router.post("/signup",async(req,res)=>{
 })
 
 
+router.post("/signin",async(req,res)=>{
+
+    let input=req.body
+    let emailid=req.body.emailid
+    let data=await userModel.findOne({"emailid":emailid})
+    if(!data)
+    {
+        return res.json({
+            status:"Invalid user"
+        })
+    }
+   console.log(data)
+    let dbPassword=data.password
+    let inputPassword=req.body.password
+    console.log(dbPassword)
+    console.log(inputPassword)
+    const match=await bcrypt.compare(inputPassword,dbPassword)
+    if(!match)
+    {
+        return res.json({
+            status:"incorrect password"
+        })
+    }
+    res.json({
+        status:"success"
+    })
+
+
+})
+
+
 
 module.exports=router
