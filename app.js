33b1b0f2c2819e40722a2b67f970cd05d64e344e
gowrig410 <gowriganesh410@gmail.com>
2024-02-09 10:12:44 +0530

    user added

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+const express=require("express")
+const cors=require("cors")
+const mongoose=require("mongoose")
 
+const userRoute=require("./controllers/userRouter")
+
+const app=express()
+
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect("mongodb+srv://gowri:gowri123@cluster0.ysfaxj8.mongodb.net/userDb?retryWrites=true&w=majority",
+{useNewUrlParser:true}
+)
+
+app.use("/api/user",userRoute)
+
+
+app.listen(3002,()=>{
+    console.log("Server Running")
+})
